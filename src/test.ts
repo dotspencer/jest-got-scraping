3b8cd9bfe24f12448235ae40155f8e0026dd1490
Spencer Smith <dontaskspencer@gmail.com>
2024-02-29 12:14:06 -0700

    Re-enable failure case

--- src/test.ts
+++ src/test.ts
@@ -1,22 +1,22 @@
-// import { gotScraping } from 'got-scraping';
-import axios from 'axios';
+import { gotScraping } from 'got-scraping';
+// import axios from 'axios';
 
 describe('got-scraping', () => {
   test('example', async () => {
     expect(5).toBe(5);
   });
 
-  // test('status 200', async () => {
-  //   const url = 'https://apify.com/';
-  //   const res = await gotScraping.get(url);
-  //   expect(res.statusCode).toBe(200);
-  // });
-
   test('status 200', async () => {
     const url = 'https://apify.com/';
-    const res = await axios.get(url);
-    expect(res.status).toBe(200);
+    const res = await gotScraping.get(url);
+    expect(res.statusCode).toBe(200);
   });
+
+  // test('status 200', async () => {
+  //   const url = 'https://apify.com/';
+  //   const res = await axios.get(url);
+  //   expect(res.status).toBe(200);
+  // });
 });
 
 
